253acdec5082e53e81aa5b208398a8c5b2723f55
Carlos Gallo <carlos.gallo@somagrupo.com.br>
2021-06-23 12:49:31 -0300

    firts commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { 
+  View, 
+  Text, 
+  Image,
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
+import {useNavigation} from '@react-navigation/native'
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+export function SignIn(){
+
+  const navigation = useNavigation()
+  function handleSignIn(){
+    navigation.navigate('Home')
+  }
+
+  return(
+    <View style={styles.container}>
+      
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image} 
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {`\n`}
+          e organize {`\n`} 
+          suas jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`} 
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+          onPress={handleSignIn}
+        />                
+               
+      </View>
+    </View>
+  );
+}
