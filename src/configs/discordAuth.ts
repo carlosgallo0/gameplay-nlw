01dac7d3f6b21ad543c48641b79cac0969381ec8
Carlos Gallo <carlos.gallo@somagrupo.com.br>
2021-06-26 15:59:56 -0300

    aula4

--- src/configs/discordAuth.ts
+++ src/configs/discordAuth.ts
@@ -1,8 +1,8 @@
 
-const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-bf9ea97f-dac5-425c-bcad-49aef97418fa';
+const REDIRECT_URI = 'https://auth.expo.io/@carlosgallo/gameplay';
 const SCOPE = 'identify%20connections%20email%20guilds';
 const RESPONSE_TYPE = 'token';
-const CLIENT_ID = '856925037142867979';
+const CLIENT_ID = '858375725807173633';
 const CDN_IMAGE = 'https://cdn.discordapp.com';
 
 export {
